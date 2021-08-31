dbc30b7fcfc19a471e23045d71d653e498dfeb25
Pratham Jaiswal <pr47h4m@gmail.com>
2021-08-31 20:28:28 +0530

    force redirect to https

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ const router = require('./routes/router')
 
 const app = express()
 
+app.enable('trust proxy')
 app.use(morgan('combined'))
 
 app.set('views', path.join(rootDir, 'views'))
 app.set('view engine', 'ejs')
+
+app.use(function(request, response, next) {
+
+    if (process.env.NODE_ENV != 'development' && !request.secure) {
+       return response.redirect("https://" + request.headers.host + request.url);
+    }
+
+    next();
+})
 
 app.use(express.static(path.join(rootDir, 'public')))
 
